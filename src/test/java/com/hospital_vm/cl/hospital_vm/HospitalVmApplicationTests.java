f42d8c044d96d2f5796ec981d073256ccfa16861
Jesús Vargas <jesus.vargas@tinet.cl>
2025-05-06 17:38:59 -0400

    ADD: Manejo de bd dummy en las pruebas

--- src/test/java/com/hospital_vm/cl/hospital_vm/HospitalVmApplicationTests.java
+++ src/test/java/com/hospital_vm/cl/hospital_vm/HospitalVmApplicationTests.java
@@ -2,12 +2,12 @@ package com.hospital_vm.cl.hospital_vm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HospitalVmApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
